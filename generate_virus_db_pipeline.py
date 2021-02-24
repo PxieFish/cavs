8f7c58dd69de9cec583082c5e220793bfcb74813
cavs <cavs@shangri-la.cavs.nparks.gov.sg>
2021-02-24 15:53:27 +0800

    added script for generating pipeline files for updating virus database

---
+++ generate_virus_db_pipeline.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Generates a pipeine for downloading data from GenBank and RefSeq.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+print(args.accumulate(args.integers))
+
+# get files from 
+
+#make temporary directoty
+
+
+#download files to temporary directoy
+#curl ftp://ftp.ncbi.nlm.nih.gov/genbank/ --user ftp: > files.list.txt
+
+#get 
+
+
+
+curl ftp://ftp.ncbi.nlm.nih.gov/genbank/ --user ftp: > files.list.txt
+
+
+cat files.list.txt   | grep -P "gbvrl\d+.seq.gz"  | grep gbvrl1.seq.gz
+
+
+wget ftp://ftp.ncbi.nlm.nih.gov/genbank/gbvrl1.seq.gz
+
+
+f = open("demofile3.txt", "w")
+f.write("Woops! I have deleted the content!")
+f.close()
