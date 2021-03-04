27388db5467f0b842a1b818bd5ffb6738884f911
Adrian Tan <atks@umich.edu>
2021-03-04 18:00:23 +0800

    misc changes

--- generate_virus_db_pipeline.py
+++ generate_virus_db_pipeline.py
@@ -2,6 +2,7 @@
 
 import argparse
 import subprocess
+import os
 
 parser = argparse.ArgumentParser(description='Generates a pipeine for downloading data from GenBank and RefSeq.')
 parser.add_argument('integers', metavar='N', type=int, nargs='+',
@@ -12,6 +13,11 @@ parser.add_argument('--sum', dest='accumulate', action='store_const',
 
 args = parser.parse_args()
 print(args.accumulate(args.integers))
+
+#get genbanks
+result = subprocess.run("curl ftp://ftp.ncbi.nlm.nih.gov/genbank/GB_Release_Number --user ftp:".split(), stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+type(int(result.stdout.decode("utf-8").split("\n")[3]))
+result.returncode
 
 # get files from 
 
@@ -26,9 +32,34 @@ print(args.accumulate(args.integers))
 #get 
 #curl ftp://ftp.ncbi.nlm.nih.gov/genbank/GB_Release_Number  --user ftp:
 
-version = subprocess.run(["curl", "ftp://ftp.ncbi.nlm.nih.gov/genbank/GB_Release_Number", "--user", "ftp:"], capture_output=True)
-print("gb version:", version)
+#version = subprocess.run(["curl", "ftp://ftp.ncbi.nlm.nih.gov/genbank/GB_Release_Number", "--user", "ftp:"], 
+#                           stdout=subprocess.PIPE, stderr=os.devnull)
+#print("gb version:", version)
 	
+def run(cmd):
+    os.environ['PYTHONUNBUFFERED'] = "1"
+    proc = subprocess.Popen(cmd,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.STDOUT,
+    )
+    stdout, stderr = proc.communicate()
+ 
+    return proc.returncode, stdout, stderr
+ 
+code, out, err = run("curl ftp://ftp.ncbi.nlm.nih.gov/genbank/GB_Release_Number --user ftp:".split())
+
+
+result = subprocess.run("curl ftp://ftp.ncbi.nlm.nih.gov/genbank/GB_Release_Number --user ftp:".split(), stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+type(int(result.stdout.decode("utf-8").split("\n")[3]))
+result.returncode
+
+ 
+print("out: '{}'".format(out))
+print("err: '{}'".format(err))
+print("exit: {}".format(code))
+ 
+print(out.split("\n")[1]) 
+#print("GB version: '{}'".format(out.split("\n")[1]))
 	
 #out = subprocess.run(["curl", "ftp://ftp.ncbi.nlm.nih.gov/genbank/", "--user", "ftp:"], capture_output=True)
 #print("program output:", out)
--- update_genbank_virus_db.py
+++ update_genbank_virus_db.py
@@ -1,48 +1,123 @@
-#Brief workflow for Nanopore data analysis 
-#
-#Workflow 1
-#Basecalling using Guppy (to convert FAST5 file to FASTQ file) → Debarcode the samples using Guppy  → Map the reads to all viruses in GenBank using minimap2 → Extract the reads which map to targeted virus genome  → assemble the reads into contigs using Canu →  Select the best contig by number of mapped reads used for assembly (if there are multiple to represent the segment) → Map the complete sample reads to the new assembled best contigs to determine read mapping percentages as well as coverage to the assembled contigs
-#Workflow 2
-#Basecalling using Guppy (to convert FAST5 file to FASTQ file) → Debarcode the samples using Guppy  → Genome Detective using Fastq files
-#Basecalling by Guppy
-#
-#Guppy codes
-#
-#Bernet’s codes: 
-#
-#
-#Nanopore community:
-#guppy_basecaller --input_path /data/my_folder/reads --save_path /data/output_folder/basecall --flowcell FLO-MIN106 --kit SQK-LSK109
-#
-#Test Guppy run of AHSV FAST5 data run on 16 April 2020
-#guppy_basecaller -i /var/lib/MinKNOW/data/20200416/AHSV-Bento/20200416_0647_MN29953_FAL08047_5aa03c6b -r -s OUTPUT/var/lib/Minknow/data/20200416 --flowcell FLO-MIN106 --kit SQK-LSK109 --num_callers 12 --cpu_threads_per_caller 2
-#Notes: FAST5 files are located at /var/lib/MinKNOW/data/20200416/AHSV-Bento/20200416_0647_MN29953_FAL08047_5aa03c6b 
-#OUTPUT files are at Home/OUTPUT
-#Highlighted part is not necessary
-#
-#Demultiplexing by Guppy
-#
-#Bernet’s codes: 
-#guppy_barcoder -i OUTPUT_DIR -s OUTPUT_DEBARCODE_DIR --barcode_kits BARCODE_KIT -t 12
-#Nanopore community:
-#guppy_barcoder --input_path <folder containing FASTQ files> --save_path <output folder> --config configuration.cfg --barcode_kits EXP-NBD104
-#Test debarcode run of 28 FASTQ files on 15 May 2020
-#guppy_barcoder -i OUTPUT -s OUTPUT3 --barcode_kits EXP-NBD104 -t 12
-#Notes: Output file is at HOME/OUTPUT3; FastQ files are at Home/OUTPUT
-#Barcode 01, 02, 03 are seperated.  
-#
-#Combine FastQ files into one file
-#
-#
-#Transfe the above scripts to the demultiplexed folder
-#Open Terminal at this folder
-#Type Code: dos2unix concatAndBGzipNanoporeDeBarcodedFastqFiles.sh
-#Type Code: bash concatAndBGzipNanoporeDeBarcodedFastqFiles.sh
-#A combind Fastq file will be generated in this folder
-#
-#Fastq file can be uploaded to Genome Detective Virus Tool for analysis (www.genomedetective.com)
-#
-#
-ONT calling
+/* The MIT License
+   Copyright (c) 2021 Adrian Tan <adrian_tan@nparks.gov.sg>
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*/
+#!/usr/bin/env python3
 
-nohup /usr/local/ont-guppy-cpu/bin/guppy_basecaller -i /data/ngs/ONT11/fast5 -r -s /home/cavs/20200226_nanopore_fastq_generation/gbc_output --flowcell FLO-MIN106 --kit SQK-LSK109 --num_callers 4 --cpu_threads_per_caller 2 > gbc.log 2> gbc.err &
+import argparse
+import subprocess
+import os
+
+parser = argparse.ArgumentParser(description='Generates a pipeline for downloading virus data from GenBank.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+print(args.accumulate(args.integers))
+    
+ 
+#get genbank current release version
+result = subprocess.run("curl ftp://ftp.ncbi.nlm.nih.gov/genbank/GB_Release_Number --user ftp:".split(), stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+if results.returncode == 0:
+
+
+type(int(result.stdout.decode("utf-8").split("\n")[3]))
+result.returncode
+
+#download to /data/ref/virus/genbank/<release_version>
+
+#if all done. no need to download
+
+
+#
+from Bio import SeqIO
+import os
+
+outfile = open("genbank_viruses.fa", "w")
+
+for path in os.listdir("."):
+	if path.startswith("gbvrl") and path.endswith(".seq.gz"):
+		print path
+		os.system("gunzip -c %s > test.gb" % path)
+		for record in SeqIO.parse("test.gb", "genbank"):
+			print record.description
+			outfile.write(">%s %s\n%s\n" % (record.id, record.description, record.seq))
+
+outfile.close()
+
+os.unlink("test.gb")
+
+
+
+# get files from 
+
+#make temporary directory
+
+#out = subprocess.run(["cat", "/home/cavs/demofile3.txt"], capture_output=True)
+#print("program output:", out)
+
+
+#create temporary files directory
+
+
+#download files to temporary directoy
+#curl ftp://ftp.ncbi.nlm.nih.gov/genbank/ --user ftp: > files.list.txt
+
+	
+def run(cmd):
+    os.environ['PYTHONUNBUFFERED'] = "1"
+    proc = subprocess.Popen(cmd,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.STDOUT,
+    )
+    stdout, stderr = proc.communicate()
+ 
+    return proc.returncode, stdout, stderr
+ 
+code, out, err = run("curl ftp://ftp.ncbi.nlm.nih.gov/genbank/GB_Release_Number --user ftp:".split())
+
+
+result = subprocess.run("curl ftp://ftp.ncbi.nlm.nih.gov/genbank/GB_Release_Number --user ftp:".split(), stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+type(int(result.stdout.decode("utf-8").split("\n")[3]))
+result.returncode
+
+ 
+print("out: '{}'".format(out))
+print("err: '{}'".format(err))
+print("exit: {}".format(code))
+ 
+print(out.split("\n")[1]) 
+#print("GB version: '{}'".format(out.split("\n")[1]))
+	
+#out = subprocess.run(["curl", "ftp://ftp.ncbi.nlm.nih.gov/genbank/", "--user", "ftp:"], capture_output=True)
+#print("program output:", out)
+
+
+#def execute() :
+	
+
+#curl ftp://ftp.ncbi.nlm.nih.gov/genbank/ --user ftp: > files.list.txt
+#cat files.list.txt   | grep -P "gbvrl\d+.seq.gz"  | grep gbvrl1.seq.gz
+#wget ftp://ftp.ncbi.nlm.nih.gov/genbank/gbvrl1.seq.gz
+
+
+#f = open("demofile3.txt", "w")
+#f.write("Woops! I have deleted the content!")
+#f.close()
