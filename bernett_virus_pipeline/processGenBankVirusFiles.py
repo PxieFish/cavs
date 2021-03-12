52df4fe4cfef59f538cfe2d837e0bc34925093a4
Adrian Tan <atks@umich.edu>
2021-03-12 17:08:19 +0800

    minor changes to GB file procesing script

--- bernett_virus_pipeline/processGenBankVirusFiles.py
+++ bernett_virus_pipeline/processGenBankVirusFiles.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from Bio import SeqIO
 import os
@@ -7,10 +7,10 @@ outfile = open("genbank_viruses.fa", "w")
 
 for path in os.listdir("."):
 	if path.startswith("gbvrl") and path.endswith(".seq.gz"):
-		print path
+		print(path)
 		os.system("gunzip -c %s > test.gb" % path)
 		for record in SeqIO.parse("test.gb", "genbank"):
-			print record.description
+			#print(record.description)
 			outfile.write(">%s %s\n%s\n" % (record.id, record.description, record.seq))
 
 outfile.close()
