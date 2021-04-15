51532deb0802d315edc84abbd258d473cca203a0
Adrian Tan <atks@umich.edu>
2021-04-15 09:46:20 +0800

    snakemake updates

--- generate_virus_db_pipeline.py
+++ generate_virus_db_pipeline.py
@@ -4,38 +4,36 @@ import argparse
 import subprocess
 import os
 
-parser = argparse.ArgumentParser(description='Generates a pipeine for downloading data from GenBank and RefSeq.')
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='an integer for the accumulator')
-parser.add_argument('--sum', dest='accumulate', action='store_const',
-                    const=sum, default=max,
-                    help='sum the integers (default: find the max)')
+parser = argparse.ArgumentParser(description='Generates a pipeine for downloading viral sequences from GenBank and RefSeq.')
+#parser.add_argument('integers', metavar='N', type=int, nargs='+',
+  #                  help='an integer for the accumulator')
+#parser.add_argument('--sum', dest='accumulate', action='store_const',
+ #                   const=sum, default=max,
+ #                   help='sum the integers (default: find the max)')
 
 args = parser.parse_args()
-print(args.accumulate(args.integers))
+#print(args.accumulate(args.integers))
 
-#get genbanks
-result = subprocess.run("curl ftp://ftp.ncbi.nlm.nih.gov/genbank/GB_Release_Number --user ftp:".split(), stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
-type(int(result.stdout.decode("utf-8").split("\n")[3]))
-result.returncode
+#get genbank version number
+print("Getting version number from genbank ... ", flush=True ,end=" ")
+#result = subprocess.check_output("curl ftp://ftp.ncbi.nlm.nih.gov/genbank/GB_Release_Number --user ftp:".split(), 
+#                                 stderr = subprocess.DEVNULL)
+result = 242
+print(int(result))
 
-# get files from 
+#create directories
+path = "data/ref/virus/genbank/" + str(int(result))
+if not os.path.exists(path):
+    print("Creating directory ...  ", path)
+    try:
+        os.makedirs(path)
+    except OSError:
+        print("Creation of %s failed" % path)
+    else:
+        print("Creation of %s successful" % path)
+else:
+    print(path, "exists")  
 
-#make temporary directory
-
-#out = subprocess.run(["cat", "/home/cavs/demofile3.txt"], capture_output=True)
-#print("program output:", out)
-
-#download files to temporary directoy
-#curl ftp://ftp.ncbi.nlm.nih.gov/genbank/ --user ftp: > files.list.txt
-
-#get 
-#curl ftp://ftp.ncbi.nlm.nih.gov/genbank/GB_Release_Number  --user ftp:
-
-#version = subprocess.run(["curl", "ftp://ftp.ncbi.nlm.nih.gov/genbank/GB_Release_Number", "--user", "ftp:"], 
-#                           stdout=subprocess.PIPE, stderr=os.devnull)
-#print("gb version:", version)
-	
 def run(cmd):
     os.environ['PYTHONUNBUFFERED'] = "1"
     proc = subprocess.Popen(cmd,
@@ -46,19 +44,19 @@ def run(cmd):
  
     return proc.returncode, stdout, stderr
  
-code, out, err = run("curl ftp://ftp.ncbi.nlm.nih.gov/genbank/GB_Release_Number --user ftp:".split())
+#code, out, err = run("curl ftp://ftp.ncbi.nlm.nih.gov/genbank/GB_Release_Number --user ftp:".split())
 
 
-result = subprocess.run("curl ftp://ftp.ncbi.nlm.nih.gov/genbank/GB_Release_Number --user ftp:".split(), stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
-type(int(result.stdout.decode("utf-8").split("\n")[3]))
-result.returncode
+#result = subprocess.run("curl ftp://ftp.ncbi.nlm.nih.gov/genbank/GB_Release_Number --user ftp:".split(), stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+#type(int(result.stdout.decode("utf-8").split("\n")[3]))
+#result.returncode
 
  
-print("out: '{}'".format(out))
-print("err: '{}'".format(err))
-print("exit: {}".format(code))
- 
-print(out.split("\n")[1]) 
+#print("out: '{}'".format(out))
+#print("err: '{}'".format(err))
+#print("exit: {}".format(code))
+# 
+#print(out.split("\n")[1]) 
 #print("GB version: '{}'".format(out.split("\n")[1]))
 	
 #out = subprocess.run(["curl", "ftp://ftp.ncbi.nlm.nih.gov/genbank/", "--user", "ftp:"], capture_output=True)
